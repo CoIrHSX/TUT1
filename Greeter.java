b36b622f5c85395cb4c1fba99da0e841e39118e7
Sixun Hao <2559510115@qq.com>
2023-09-07 17:56:11 +0800

    Added feature 2

--- Greeter.java
+++ Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
 
   public String sayHello() {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
-    return messages.getString("greeting11111") + ", " + name;
+    return messages.getString("greeting11111") + ", " + "+name+" ;
   }
 
   public static void main(String[] args) {
